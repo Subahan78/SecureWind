41ec5e2d18335c59edb0ba146ec137c061bc65b2
Subahan78 <subahanshaik.tech@gmail.com>
2026-07-21 21:22:20 +0530

    Update ids.py

--- security/ids.py
+++ security/ids.py
@@ -1,48 +1,24 @@
-# ==========================================
-# SecureWind Intrusion Detection System (IDS)
-# ==========================================
-
 def detect_attack(username, password):
-
     attack_patterns = [
-
         "'",
-
         "--",
-
         ";",
-
         "<script>",
-
         "</script>",
-
         "../",
-
         "&&",
-
         "|",
-
         " OR ",
-
         "SELECT",
-
         "DROP",
-
         "DELETE",
-
         "INSERT",
-
         "UPDATE"
-
     ]
-
     username_upper = username.upper()
     password_upper = password.upper()
-
     for attack in attack_patterns:
-
         if attack.upper() in username_upper or attack.upper() in password_upper:
-
             print("\n===================================")
             print("      SECURITY ALERT")
             print("===================================")
@@ -50,18 +26,10 @@ def detect_attack(username, password):
             print("Blocked Pattern:", attack)
             print("Access Denied")
             print("===================================")
-
             return True
-
     return False
-
-
-# Test IDS
-
 if __name__ == "__main__":
-
     username = input("Enter Username: ")
     password = input("Enter Password: ")
-
     if not detect_attack(username, password):
         print("\nNo attack detected.")
